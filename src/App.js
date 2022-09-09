6e764abe5bc4a51233222ff013ad9516b61e6281
Keerthana215 <keerthana.g.s.99@gmail.com>
2022-09-08 23:50:15 -0700

    Added inventory table

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import EnhancedTable, {ResponsiveAppBar} from './invNav';
+
+export default function App() {
+  return (
+    <div>
+        <ResponsiveAppBar />
+        <EnhancedTable />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './SignIn';
+// import { StyledEngineProvider } from '@mui/material/styles';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

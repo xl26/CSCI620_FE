05450b74d310dad8542050710bf59d117bf8a13e
Keerthana215 <keerthana.g.s.99@gmail.com>
2022-09-16 21:26:25 -0700

    Added SignUp page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import * as React from "react";
 import EnhancedTable, {ResponsiveAppBar} from './invNav';
+// import SignInSide from "./SignIn";
 
 export default function App() {
   return (
     <div>
+      {/* <SignInSide/> */}
         <ResponsiveAppBar />
         <EnhancedTable />
     </div>

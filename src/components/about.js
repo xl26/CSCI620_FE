d7f00c7eeffb3da56c8cc3f2d32498816781648b
sahildps <sahildshetty@gmail.com>
2022-09-21 10:38:25 -0700

    edit copyright

--- src/components/about.js
+++ src/components/about.js
@@ -8,7 +8,7 @@ import Link from '@mui/material/Link';
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary">
-      {'Copyright © '}
+      {'Copyright  '}
       <Link color="inherit" href="https://mui.com/">
         Your Website
       </Link>{' '}
